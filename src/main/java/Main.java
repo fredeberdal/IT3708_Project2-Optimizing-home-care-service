277f3c808ba5d900237da2c8836b1fd537041296
Frede <frede.berdal@gmail.com>
2023-03-16 15:22:02 +0100

    lok

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,12 +7,10 @@ import org.json.simple.parser.ParseException;
 import service.PopulationGenerator;
 import utils.VRPFileReader;
 
-import javax.swing.*;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class Main {
 
@@ -25,30 +23,32 @@ public class Main {
         patientList = reader.fetchPatients();
         reader.fetchData(obj);
         reader.fetchTravelMatrix(obj);
-
         PopulationGenerator pg = new PopulationGenerator();
-        Nurse nurse = new Nurse(1, Settings.nurse_capacity, patientList);
-        System.out.println(nurse.getCapacity());
-        System.out.println("PENIS");
-        nurse.setCapacity(10);
-        System.out.println(nurse.getCapacity());
-        System.out.println("bajs");
-        nurse.setCapacity(10);
-        System.out.println(nurse.getCapacity());
-        System.out.println("TESS");
-        //List<Nurse> list = pg.generateRandom(patientList, Settings.number_of_nurses);
+
+
+        List<Nurse> list = pg.generateRandom(patientList, Settings.number_of_nurses);
+        double time = 0;
+        for (Nurse nurse : list) {
+            System.out.println("Find max_travel_time");
+            if (time < nurse.getTime_traveled()) {
+                time = nurse.getTime_traveled();
+            }
+        }
+        Settings.setTotal_travel_time(time);
+
 
         int counter = 0;
-        /*
         for (Nurse nurse : list) {
-             System.out.println("New nurse");
-             for(Patient p : nurse.getListOfPatients()){
-                 System.out.println(p.getId());
-                 counter++;
-             }
+            System.out.println("New nurse");
+            for (Patient p : nurse.getListOfPatients()) {
+                System.out.println(p.getId());
+                counter++;
+            }
         }
-         */
+
+
         System.out.println("siste");
         System.out.println(counter);
+
     }
 }
